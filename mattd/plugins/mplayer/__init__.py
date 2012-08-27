bff8010ebf66cd67d1696026b753945cdd92c99f
Ralph Bean <rbean@redhat.com>
2012-08-27 16:07:45 -0400

    Draw values from config files.  Fixes #1.

--- mattd/plugins/mplayer/__init__.py
+++ mattd/plugins/mplayer/__init__.py
@@ -1,3 +1,4 @@
+import os
 import time
 
 sh, pbs = None, None
@@ -6,29 +7,41 @@ try:
 except ImportError:
     import pbs
 
-
-# TODO - Obviously, put these in a config file...
-KEYPHRASE = ""
-sound_file = "trololo.ogg"
-
-
-def play(sound_file):
-    if pbs:
-        print pbs.mplayer("-quiet", sound_file)
-    else:
-        print sh.mplayer("-quiet", sound_file)
+import logging
+log = logging.getLogger("mattd")
 
 
 class MPlayerPlugin(object):
     def __init__(self, app):
+        # First, do some validation of our config file.  Got what we need?
+        if not __name__ in app.config.sections():
+            raise ValueError("Couldn't find [%s] in config" % __name__)
+
+        for attr in ['keyphrase', 'target']:
+            if not app.config.has_option(__name__, attr):
+                raise ValueError("[%s] has no %r" % (__name__, attr))
+
+            setattr(self, attr, app.config.get(__name__, attr))
+
+        # Furthermore, make sure the target is a real file.
+        self.target = os.path.abspath(os.path.expanduser(self.target))
+        if not os.path.exists(self.target):
+            raise OSError("%r does not exist" % self.target)
+
         self.app = app
 
     def matches_keyphrase(self, phrase):
-        return KEYPHRASE in phrase
+        return self.keyphrase in phrase
 
     def go_idle(self):
         self.app.active_plugin = None
 
     def handle(self, phrase):
-        if KEYPHRASE in phrase:
-            play(sound_file)
+        if self.keyphrase in phrase:
+            self.play()
+
+    def play(self):
+        if pbs:
+            log.debug(pbs.mplayer("-quiet", self.target))
+        else:
+            log.debug(sh.mplayer("-quiet", self.target))
